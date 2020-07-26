9f1f639f3cd092308a071b7df30fe143dfe55335
Keno Budde <Clearwood@users.noreply.github.com>
2020-07-26 23:33:30 +0200

    aktuell

--- src/App.js
+++ src/App.js
@@ -1,281 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Graph from "./graph.js";
+import { Input } from "antd";
+
 function App() {
-  const data = [
-    {
-      id: "japan",
-      color: "hsl(335, 70%, 50%)",
-      data: [
-        {
-          x: "plane",
-          y: 147,
-        },
-        {
-          x: "helicopter",
-          y: 284,
-        },
-        {
-          x: "boat",
-          y: 125,
-        },
-        {
-          x: "train",
-          y: 20,
-        },
-        {
-          x: "subway",
-          y: 51,
-        },
-        {
-          x: "bus",
-          y: 58,
-        },
-        {
-          x: "car",
-          y: 265,
-        },
-        {
-          x: "moto",
-          y: 238,
-        },
-        {
-          x: "bicycle",
-          y: 9,
-        },
-        {
-          x: "horse",
-          y: 8,
-        },
-        {
-          x: "skateboard",
-          y: 44,
-        },
-        {
-          x: "others",
-          y: 40,
-        },
-      ],
-    },
-    {
-      id: "france",
-      color: "hsl(107, 70%, 50%)",
-      data: [
-        {
-          x: "plane",
-          y: 73,
-        },
-        {
-          x: "helicopter",
-          y: 151,
-        },
-        {
-          x: "boat",
-          y: 227,
-        },
-        {
-          x: "train",
-          y: 111,
-        },
-        {
-          x: "subway",
-          y: 181,
-        },
-        {
-          x: "bus",
-          y: 16,
-        },
-        {
-          x: "car",
-          y: 256,
-        },
-        {
-          x: "moto",
-          y: 97,
-        },
-        {
-          x: "bicycle",
-          y: 200,
-        },
-        {
-          x: "horse",
-          y: 95,
-        },
-        {
-          x: "skateboard",
-          y: 298,
-        },
-        {
-          x: "others",
-          y: 204,
-        },
-      ],
-    },
-    {
-      id: "us",
-      color: "hsl(271, 70%, 50%)",
-      data: [
-        {
-          x: "plane",
-          y: 203,
-        },
-        {
-          x: "helicopter",
-          y: 109,
-        },
-        {
-          x: "boat",
-          y: 277,
-        },
-        {
-          x: "train",
-          y: 52,
-        },
-        {
-          x: "subway",
-          y: 88,
-        },
-        {
-          x: "bus",
-          y: 90,
-        },
-        {
-          x: "car",
-          y: 20,
-        },
-        {
-          x: "moto",
-          y: 168,
-        },
-        {
-          x: "bicycle",
-          y: 41,
-        },
-        {
-          x: "horse",
-          y: 5,
-        },
-        {
-          x: "skateboard",
-          y: 62,
-        },
-        {
-          x: "others",
-          y: 11,
-        },
-      ],
-    },
-    {
-      id: "germany",
-      color: "hsl(201, 70%, 50%)",
-      data: [
-        {
-          x: "plane",
-          y: 85,
-        },
-        {
-          x: "helicopter",
-          y: 100,
-        },
-        {
-          x: "boat",
-          y: 53,
-        },
-        {
-          x: "train",
-          y: 123,
-        },
-        {
-          x: "subway",
-          y: 80,
-        },
-        {
-          x: "bus",
-          y: 280,
-        },
-        {
-          x: "car",
-          y: 154,
-        },
-        {
-          x: "moto",
-          y: 168,
-        },
-        {
-          x: "bicycle",
-          y: 33,
-        },
-        {
-          x: "horse",
-          y: 96,
-        },
-        {
-          x: "skateboard",
-          y: 245,
-        },
-        {
-          x: "others",
-          y: 239,
-        },
-      ],
-    },
-    {
-      id: "norway",
-      color: "hsl(96, 70%, 50%)",
-      data: [
-        {
-          x: "plane",
-          y: 45,
-        },
-        {
-          x: "helicopter",
-          y: 21,
-        },
-        {
-          x: "boat",
-          y: 33,
-        },
-        {
-          x: "train",
-          y: 28,
-        },
-        {
-          x: "subway",
-          y: 238,
-        },
-        {
-          x: "bus",
-          y: 242,
-        },
-        {
-          x: "car",
-          y: 148,
-        },
-        {
-          x: "moto",
-          y: 64,
-        },
-        {
-          x: "bicycle",
-          y: 22,
-        },
-        {
-          x: "horse",
-          y: 224,
-        },
-        {
-          x: "skateboard",
-          y: 89,
-        },
-        {
-          x: "others",
-          y: 281,
-        },
-      ],
-    },
-  ];
-  return <Graph data={data} />;
+  const [data, setData] = useState({
+    cases: [
+      {
+        x: "14/06/2020",
+        y: "10",
+      },
+      {
+        x: "13/06/2020",
+        y: "20",
+      },
+    ],
+    deaths: [
+      {
+        x: "14/06/2020",
+        y: "5",
+      },
+      {
+        x: "13/06/2020",
+        y: "20",
+      },
+    ],
+    geoId: "AF",
+  });
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("localhost:5000/country", requestOptions)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+    // Run! Like go get some data from an API.
+  }, []);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const value = e.target.value;
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("localhost:5000/cases/byCountry/" + value, requestOptions)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <Input placeholder="country" />
+      </form>
+      <Graph data={data} />
+    </div>
+  );
 }
 
 export default App;
